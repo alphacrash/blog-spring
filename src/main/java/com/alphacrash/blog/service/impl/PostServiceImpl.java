254e8416aa205a2063c49979e6ade370402fc1eb
SUD <sudhansu.ace@gmail.com>
2022-07-26 23:05:29 +0530

    Updated Blog Application

--- src/main/java/com/alphacrash/blog/service/impl/PostServiceImpl.java
+++ src/main/java/com/alphacrash/blog/service/impl/PostServiceImpl.java
@@ -6,6 +6,7 @@ import com.alphacrash.blog.payload.PostDto;
 import com.alphacrash.blog.payload.PostResponse;
 import com.alphacrash.blog.repository.PostRepository;
 import com.alphacrash.blog.service.PostService;
+import org.modelmapper.ModelMapper;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -19,9 +20,11 @@ import java.util.stream.Collectors;
 public class PostServiceImpl implements PostService {
 
     private final PostRepository postRepository;
+    private ModelMapper mapper;
 
-    public PostServiceImpl(PostRepository postRepository) {
+    public PostServiceImpl(PostRepository postRepository, ModelMapper mapper) {
         this.postRepository = postRepository;
+        this.mapper = mapper;
     }
 
     @Override
@@ -73,20 +76,13 @@ public class PostServiceImpl implements PostService {
 
     // Convert Entity to DTO
     private PostDto mapToDTO(Post post) {
-        PostDto postDto = new PostDto();
-        postDto.setId(post.getId());
-        postDto.setTitle(post.getTitle());
-        postDto.setDescription(post.getDescription());
-        postDto.setContent(post.getContent());
+        PostDto postDto = mapper.map(post, PostDto.class);
         return postDto;
     }
 
     // Convert DTO to Entity
     private Post mapToEntity(PostDto postDto) {
-        Post post = new Post();
-        post.setTitle(postDto.getTitle());
-        post.setDescription(postDto.getDescription());
-        post.setContent(postDto.getContent());
+        Post post = mapper.map(postDto, Post.class);
         return post;
     }
 }
